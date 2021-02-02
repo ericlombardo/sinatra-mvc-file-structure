4cda2d4de3f971016ba11705469a3d9f378c3369
Eric <eric.m.lombardo@gmail.com>
2021-02-02 14:00:27 -0500

    Create Dog model class for MVC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+    set :views, "app/views"   # where to find the view files (HTML) to display
+  	set :public_dir, "public" # where the public directory is
   end
 
   get "/" do
